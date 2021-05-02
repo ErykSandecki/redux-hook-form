811eefab4d0e617c912dad50b8eca8e523be4745
ErykSandecki <eryk.sandecki@gmail.com>
2021-05-02 17:20:29 +0200

    add actions to export

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
 export { reduxHookForm } from './reducer';
 export { REDUCER_KEY } from './actionsType';
+export { submit, submitError, submitSuccess } from './actions';
 export type { TReduxHookFormState } from './types';
